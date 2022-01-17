65cb7643de08d522c71af92b09a6fcc2944036b1
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-01-17 20:39:35 +0200

    second task complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import trottle from 'lodash.throttle';
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
 
+const LOCALSTORAGE_KAY = 'videoplayer-current-time';
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KAY));
+
+player.on('timeupdate', trottle(onTimeUpdate, 1000));
+console.log(player.getCurrentTime());
+
+function onTimeUpdate(ev) {
+  const currentTime = ev.seconds;
+  localStorage.setItem(LOCALSTORAGE_KAY, currentTime);
+}
