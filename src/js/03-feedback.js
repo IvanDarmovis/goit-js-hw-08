88c84ccb8ab7133094c1233fc8901fa8d8eab011
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-01-18 10:48:36 +0200

    third task complete

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import trottle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form [name=email]'),
+  message: document.querySelector('.feedback-form [name=message]'),
+};
+
+const result = {};
+
+refs.form.addEventListener('input', trottle(onFormInput, 500));
+refs.form.addEventListener('submit', onSubmitForm);
+
+// const keys =
+if (localStorage.getItem('feedback-form-state')) {
+  const preloadSettings = JSON.parse(localStorage.getItem('feedback-form-state'));
+  Object.keys(preloadSettings).forEach(el => {
+    console.log(refs[el].value);
+    refs[el].value = preloadSettings[el];
+    result[el] = preloadSettings[el];
+  });
+}
+
+function onFormInput(ev) {
+  const name = ev.target.name;
+  const value = ev.target.value;
+  result[name] = value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(result));
+}
+
+function onSubmitForm(ev) {
+  ev.preventDefault();
+  console.log(result);
+  localStorage.removeItem('feedback-form-state');
+  Object.keys(result).forEach(el => {
+    refs[el].value = '';
+  });
+}
